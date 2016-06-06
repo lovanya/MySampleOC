2aaec81e8abe862cc1a7617ed46b396595c8f995
brucelin <bl1061702060@outlook.com>
2016-06-06 22:02:47 +0800

    a new project about TableView

---
+++ Simple Table/Simple Table/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple Table
+//
+//  Created by Bruce on 16/6/6.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple Table/Simple Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple Table
+//
+//  Created by Bruce on 16/6/6.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
