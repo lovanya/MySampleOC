c314d60ef1b37d88fec1284648a43cdbdf86a2e6
brucelin <bl1061702060@outlook.com>
2016-06-13 22:45:30 +0800

    增加了部分行样式的设置

--- Simple Table/Simple Table/ViewController.m
+++ Simple Table/Simple Table/ViewController.m
@@ -30,15 +30,29 @@
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
 }
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    if(section == 0){
-        return [self.dwarves count];
+
+//阻止第一行被选中（选中行高亮前调用）
+-(NSIndexPath *) tableView: (UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.row == 0){
+        return nil;
     }else{
-        return [self.dwarves count];
+        return indexPath;
     }
-    
+}
+//设置行缩进
+-(NSInteger)tableView:(UITableView*)tableView indentationLevelForRowAtIndexPath:(NSIndexPath*)indexPath{
+    return indexPath.row % 4;
 }
 
+//每个区间的行数
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.dwarves count];
+}
+
+//行高
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
+    return indexPath.row == 0 ? 100:70;
+}
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: SimpleTableIdentifier];
@@ -56,7 +70,7 @@
 //    cell.textLabel.textColor = [UIColor whiteColor];
 //    cell.textLabel.highlightedTextColor = [UIColor blackColor];
     cell.textLabel.text = self.dwarves[indexPath.row];
-    
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:30];
     if(indexPath.row < 7){
         cell.detailTextLabel.text = @"Mr.Disney";
     } else {
@@ -65,6 +79,16 @@
     return cell;
 }
 
-
+//行的选中事件(选中之后调用)
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *rowValue = self.dwarves[indexPath.row];
+    NSString *message = [[NSString alloc] initWithFormat:@"You selected %@",rowValue];
+    
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Row Selected!" message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction  *cancelAction = [UIAlertAction actionWithTitle:@"Yes I Did" style:UIAlertActionStyleDefault handler:nil];
+    [controller addAction:cancelAction];
+    [self presentViewController: controller animated:YES completion:nil];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 @end
